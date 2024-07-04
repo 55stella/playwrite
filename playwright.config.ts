0c98c38a71d9cb515a7e7bc3c9afab8180bf84ed
55stella <stellarichards7@gmail.com>
2024-07-04 09:32:07 +0100

    refactored the config file and worked on adding allure dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,10 +43,7 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    
 
     /* Test against mobile viewports. */
     // {
